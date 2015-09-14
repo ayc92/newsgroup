5c3785bdb1aff7bc26793700fde647c57767f497
Jason Chen <jasonschen5@gmail.com>
2015-09-13 18:29:13 -0700

    add Parse sdk and login logic

--- src/js/app/components/Login.jsx
+++ src/js/app/components/Login.jsx
@@ -1,24 +1,50 @@
-define(['react'], function(React) {
+define(['react', 'parse'], function(React, Parse) {
     var LoginForm = React.createClass({
-        render: function() {
+        getInitialState: function () {
+            return { email: '', password: '' };
+        },
+
+        onChange: function (e) {
+            var input = e.target;
+            if (input.getAttribute('type') === 'password')
+                this.setState({ password: input.value });
+            else
+                this.setState({ email: input.value });
+        },
+
+        login: function () {
+            Parse.User.logIn(this.state.email, this.state.password, {
+                success: function(user) {
+                    // Do stuff after successful login.
+                    console.log('login successful');
+                },
+                error: function(user, error) {
+                    // The login failed. Check error to see why.
+                    console.log('login failed: ' + error);
+                }
+            });
+        },
+
+        render: function () {
             return (
-                <form className="login">
-                    <input type="text"/>
-                    <input type="password"/>
-                </form>
+                <div className="login container">
+                    <form className="login">
+                        <input
+                            type="text"
+                            value={this.state.email}
+                            onChange={this.onChange} />
+                        <input
+                            type="password"
+                            value={this.state.password}
+                            onChange={this.onChange} />
+                        <a className="submit" onClick={this.login}>
+                            Login
+                        </a>
+                    </form>
+                </div>
             );
         }
     });
 
-    var LoginContainer = React.createClass({
-        render: function() {
-            return (
-                <div className="login container">
-                    <LoginForm/>
-                </div>
-            );
-        }
-    })
-
-    return LoginContainer;
+    return LoginForm;
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,22 @@
 require.config({
     baseUrl: 'js/',
 
+    shim: {
+        parse: {
+            deps: ['jquery', 'lodash'],
+            exports: 'Parse'
+        }
+    },
+
     paths: {
         components: 'app/components',
         react: 'libs/react-with-addons',
         jquery: 'libs/jquery-2.1.4',
         JSXTransformer: 'libs/JSXTransformer',
         jsx: 'libs/require-jsx',
-        text: 'libs/require-text'
+        text: 'libs/require-text',
+        parse: 'libs/parse-1.6.0',
+        lodash: 'libs/lodash'
     },
 
     jsx: {
@@ -15,6 +24,7 @@ require.config({
     }
 });
 
-require(['app/app'], function(App) {
+require(['app/app', 'parse'], function(App, Parse) {
     App.initialize();
+    Parse.initialize("IqAUe1un4WXYJx7fLlXPX66NJZFcvV2vihCLfb0a", "I10AAdaEo7O4sAaVH1asYztogf9ncENMlFwNe1CB");
 });
